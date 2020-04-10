f28777d810e536be5f9a6c23cdcfb1ae3ca2a0d5
oussama <oussbenfredj@gmail.com>
2020-04-10 18:30:37 +0200

    third commit

--- src/app/auth/sitter/sign-up-b/sign-up-b.component.ts
+++ src/app/auth/sitter/sign-up-b/sign-up-b.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up-b.component.scss']
 })
 export class SignUpBComponent implements OnInit {
+  cities = [
+    'Ariana', 'Beja', 'Ben Arous', 'Bizerte', 'Gabes' , 'Gafsa' , 'Jendouba' , 'Kasserine' , 'Kairouan' , 'Kebili' , 'Kef' ,
+    'Mahdia' , 'Manouba' , 'Medenine' , 'Monastir' , 'Nabeul' , 'Sfax' , 'Siliana' , 'Sidi Bouzid' , 'Sousse' , 'Tataouine' ,
+    'Tozeur' , 'Tunis' , 'Zaghouan' ];
+  startDate = new Date(1990, 0, 1);
 
   constructor() { }
 
--- src/app/profile/parent-id/parent-id.component.ts
+++ src/app/profile/parent-id/parent-id.component.ts
@@ -5,8 +5,59 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './parent-id.component.html',
   styleUrls: ['./parent-id.component.scss']
 })
-export class ParentIdComponent implements OnInit {
 
+export class ParentIdComponent implements OnInit {
+  url: string | ArrayBuffer = 'http://ssl.gstatic.com/accounts/ui/avatar_2x.png';
+  ArrayofkidsId = ['Achoik'];
+  newsLetterChecked = false;
+  checkBoxChecked = false;
+  otherSelectedChecked = false;
+
+ messages = [
+  {
+    IDSender : '1',
+    sender: 'Oussama',
+    status : 'Declined'
+  }
+];
+
+  addKid() {
+    this.ArrayofkidsId.push('oussama');
+  }
+
+  deleteMessage(i) {
+    this.messages.splice(i, 1);
+  }
+
+
+  deleteChild( i) {
+    this.ArrayofkidsId.splice(i, 1);
+  }
+
+  subscribeChange() {
+    this.newsLetterChecked = !this.newsLetterChecked;
+  }
+  checkBoxChange() {
+    this.checkBoxChecked = !this.checkBoxChecked;
+  }
+  othersSelectedChange() {
+    this.otherSelectedChecked = !this.otherSelectedChecked;
+  }
+  clickPic() {
+    document.getElementById('imageUpload').click();
+  }
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      // tslint:disable-next-line:no-shadowed-variable
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+      };
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
